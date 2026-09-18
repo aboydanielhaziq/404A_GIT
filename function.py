5ed523a0257d2b67a59130f6516f1aef029395ca
aboydanielhaziq <harithdanielsee@gmail.com>
2026-09-18 17:03:18 +0900

    軽減税率対応

--- function.py
+++ function.py
@@ -1,2 +1,9 @@
-def add_tax(price):
-    return price * 1.1
+def add_tax(price,tax_rate=0.1):
+    return int(price + price * tax_rate)
+
+print("test")
+
+if __name__ == "__main__":
+    assert add_tax(1000) == 1100
+    assert add_tax(1000,0.08) == 1080
+    print("test")
